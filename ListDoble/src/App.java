681f6076de23b3bcf558b458bb362a906e2a3df6
Arturo Loya <41841053+ArturShadow@users.noreply.github.com>
2022-07-01 15:04:14 -0600

    ListDouble Act

--- ListDoble/src/App.java
+++ ListDoble/src/App.java
@@ -1,5 +1,18 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        
+        var leer = new BufferedReader(new InputStreamReader(System.in));
+
+        int n,a;
+        ListaDoble listaDoble = new ListaDoble();
+        System.out.print("Cuantos elementos deseas agregar? ");
+        n = Integer.parseInt(leer.readLine());
+        for (int i = 0; i < n; i++) {
+            a = Integer.parseInt(leer.readLine());
+            listaDoble.addOrden(a);
+        }
+        listaDoble.mostrarD();
     }
 }
