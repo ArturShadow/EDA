b30702eae971264b624dbc7079dc025f138db95a
Arturo Loya <41841053+ArturShadow@users.noreply.github.com>
2022-08-06 00:52:22 -0600

    Practica 23

--- Arbol/src/App.java
+++ Arbol/src/App.java
@@ -1,17 +1,9 @@
 public class App {
     public static void main(String[] args) throws Exception {
         Arbol_Binario arbol_Binario = new Arbol_Binario();
-
-        int[] numeros = {10, 15, 4, 1, 6, 17, 19, 45, 23, 31};
-        for (int numero : numeros) {
-            arbol_Binario.Insertar(numero);
-        }
-
-        System.out.println("Orden preorden");
-        arbol_Binario.recorrerPreOrden();
-        System.out.println("\nOrden inorden");
-        arbol_Binario.recorrerInOrden();
-        System.out.println("\nOrden postorden");
-        arbol_Binario.recorrerPostOrden();
+        Menu();
+    }
+    public static void Menu() {
+        System.out.println("Seleccione accion que desee realizar.\n1. Recorrer en Preorden\n2.Recorrer inorden\n3. Recorrer en postorden");
     }
 }
