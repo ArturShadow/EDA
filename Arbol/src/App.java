d7fc5cc7c04cdab35d495a98db4351bba87535e9
ArturShadow <luis.arturo1738@hotmail.com>
2022-08-07 00:33:25 -0600

    Practica 23

--- Arbol/src/App.java
+++ Arbol/src/App.java
@@ -1,9 +1,47 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        Arbol_Binario arbol_Binario = new Arbol_Binario();
-        Menu();
+        Scanner sc = new Scanner(System.in);
+        String resNuevoArbol = "";
+        do{
+            Arbol_Binario arbol_Binario = new Arbol_Binario();
+            String res = "";
+            int num, op=0;
+            
+            do {
+                System.out.print("Ingrese el valor: ");
+                num = sc.nextInt();
+                arbol_Binario.Insertar(num);
+                System.out.print("Continuar?");
+                res = sc.next();
+            } while (res.equals("s"));
+            do {
+                System.out.println("\n");
+                Menu();
+                op = sc.nextInt();
+                switch(op){
+                    case 1:
+                    System.out.println("Recorrido preorden");
+                    arbol_Binario.recorrerPreOrden();
+                    break;
+                    case 2:
+                    System.out.println("Recorrido inorden");
+                    arbol_Binario.recorrerInOrden();
+                    break;
+                    case 3:
+                    System.out.println("Recorrido postorden");
+                    arbol_Binario.recorrerPostOrden();
+                    break;
+                }        
+            } while (op < 4);
+            System.out.print("Quiere crear un nuevo arbol? ");
+            resNuevoArbol = sc.next();
+        } while(resNuevoArbol.equals("s"));
+        sc.close();
     }
     public static void Menu() {
-        System.out.println("Seleccione accion que desee realizar.\n1. Recorrer en Preorden\n2.Recorrer inorden\n3. Recorrer en postorden");
+        System.out.print("Seleccione accion que desee realizar.\n1. Recorrer en Preorden\n2.Recorrer inorden\n3. Recorrer en postorden\n4. Salir\nOpcion--> ");
+
     }
 }
